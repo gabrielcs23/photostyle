fd12e113fae4daf9369360cfbb88a40c0153224a
gabrielcs23 <gabrielcs23@hotmail.com>
2020-06-10 19:19:39 -0300

    Correção de acesso ao backend

--- api/src/main/java/br/com/photostyle/api/ApiApplication.java
+++ api/src/main/java/br/com/photostyle/api/ApiApplication.java
@@ -4,6 +4,10 @@ import br.com.photostyle.api.infra.amazon.AmazonConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AmazonConstants.class)
@@ -11,6 +15,16 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }

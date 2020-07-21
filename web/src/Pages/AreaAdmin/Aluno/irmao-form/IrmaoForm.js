db4a1861b1d09e3903a4694778e06253ebb66c4d
gabrielcs23 <gabrielcs23@hotmail.com>
2020-07-21 13:07:41 -0300

    Corrige atualizar foto Irmão

--- web/src/Pages/AreaAdmin/Aluno/irmao-form/IrmaoForm.js
+++ web/src/Pages/AreaAdmin/Aluno/irmao-form/IrmaoForm.js
@@ -44,8 +44,8 @@ class IrmaoForm extends Component {
         }
     }
 
-    attRelacionamento(irmaos) {
-        const irmaoRel = new IrmaoRel(irmaos, this.state.fotos, this.state.id);
+    attRelacionamento(irmaos, fotos) {
+        const irmaoRel = new IrmaoRel(irmaos, fotos, this.state.id);
         this.props.relacionarIrmao(irmaoRel);
     }
 
@@ -54,7 +54,7 @@ class IrmaoForm extends Component {
         irmaos[idx] = irmao;
         this.setState({irmaos: irmaos, addIrmaoDisabled: false, fotosDisabled: false});
 
-        this.attRelacionamento(irmaos);
+        this.attRelacionamento(irmaos, this.state.fotos);
     }
 
     adicionarIrmao() {
@@ -69,7 +69,7 @@ class IrmaoForm extends Component {
         fotos.push(foto);
         this.setState({fotos: fotos});
 
-        this.attRelacionamento();
+        this.attRelacionamento(this.state.irmaos, fotos);
     }
 
     removerFoto = (idx) => {

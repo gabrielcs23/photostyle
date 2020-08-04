630bc7e3e900372076f18f5f34f4e142f05deea1
gabrielcs23 <gabrielcs23@hotmail.com>
2020-08-04 19:55:38 -0300

    Componente para visualizar fotos

---
+++ web/src/Pages/Utils/ImgBox/ImgBox.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import M from 'materialize-css';
+import './ImgBox.scss'
+
+const ImgBox = (props) => {
+
+    let imgRef;
+
+    const box = (
+        <img
+            ref={imgBox =>{
+                imgRef = imgBox
+            }}
+            className="materialboxed responsive-img"
+            src={props.img}
+            alt="foto individual"
+        />
+    )
+
+    useEffect(() => {
+        M.Materialbox.init(imgRef);
+    }, [imgRef])
+
+    return box;
+
+}
+export default ImgBox;

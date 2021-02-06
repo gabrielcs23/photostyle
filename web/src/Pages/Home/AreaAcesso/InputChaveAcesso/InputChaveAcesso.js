4dc118046320273e42fe74e33512eced60fec32a
gabrielcs23 <gabrielcs23@hotmail.com>
2021-02-06 18:28:03 -0300

    permite submit código ao pressionar enter

--- web/src/Pages/Home/AreaAcesso/InputChaveAcesso/InputChaveAcesso.js
+++ web/src/Pages/Home/AreaAcesso/InputChaveAcesso/InputChaveAcesso.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import Cleave from 'cleave.js/react';
+import React from "react";
+import Cleave from "cleave.js/react";
 
-const InputChaveAcesso = props => {
+const InputChaveAcesso = ({ onChange, onEnterKeyDown }) => {
+  const onChangeInput = (event) => {
+    onChange(event.target.value, event.target.rawValue);
+  };
 
-    const onChange = event => {
-        props.onChange(event.target.value, event.target.rawValue);
-    };
-
-    const formatOptions = {
-        delimiter: '-',
-        blocks: [4, 6],
-        uppercase: true
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      onEnterKeyDown();
     }
+  };
 
-    return (
-        <Cleave placeholder="Insira aqui sua chave de acesso"
-                options={formatOptions}
-                onChange={onChange} />
-    );
-}
-export default InputChaveAcesso
+  const formatOptions = {
+    delimiter: "-",
+    blocks: [4, 6],
+    uppercase: true,
+  };
+
+  return (
+    <Cleave
+      placeholder="Insira aqui sua chave de acesso"
+      options={formatOptions}
+      onChange={onChangeInput}
+      onKeyDown={onKeyDown}
+    />
+  );
+};
+export default InputChaveAcesso;

d19e5898d8b1faa495c426c6360cdc1bac283885
Gabriel Costa <gabrielcs23@hotmail.com>
2023-10-22 20:17:31 -0300

    deleção de escola

--- web/src/Pages/AreaAdmin/Escola/lista-escola/EscolaCard.js
+++ web/src/Pages/AreaAdmin/Escola/lista-escola/EscolaCard.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import CardActions from '../../../Utils/CardActions/CardActions';
+import ModalConfirmarExclusao from '../../../Utils/Modal/Modal';
 
 const EscolaCard = (props) => {
     const [hovered, setHovered] = useState(false);
     const toggleHover = () => setHovered(!hovered);
 
-    const { escola } = props;
+    const { escola, loading } = props;
+    const idModal = `modal-confirmar-exclusao-${escola.id}`
 
     return (
         <div className="col s6 m4">
@@ -13,22 +15,56 @@ const EscolaCard = (props) => {
                 onMouseEnter={toggleHover}
                 onMouseLeave={toggleHover}
             >
-                
-                <CardActions podeDeletar={false} editar={props.editar} />
-                
-                <div className="card-content white-text" 
-                    onClick={props.selecionar}
-                >
-                    <div className="card-title center-align">
-                        <span>{escola.nome}</span>
-                    </div>
-                    <p className="center align">
-                        {escola.apelido ? `Apelido: ${escola.apelido}` : ''}
-                        &nbsp;
-                    </p>
-                    <p>Turmas: {escola.turmas ? escola.turmas.length : '0'}</p>
-                </div>
+                {loading
+                    ? (
+
+                        <div className="card-content white-text center-align">
+                            <div className="preloader-wrapper big active">
+                                <div className="spinner-layer spinner-yellow-only">
+                                    <div className="circle-clipper left">
+                                        <div className="circle" />
+                                    </div>
+                                    <div className="gap-patch">
+                                        <div className="circle" />
+                                    </div>
+                                    <div className="circle-clipper right">
+                                        <div className="circle" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <CardActions idModal={idModal} editar={props.editar} />
+
+                            <div className="card-content white-text"
+                                onClick={props.selecionar}
+                            >
+                                <div className="card-title center-align">
+                                    <span>{escola.nome}</span>
+                                </div>
+                                <p className="center align">
+                                    {escola.apelido ? `Apelido: ${escola.apelido}` : ''}
+                                    &nbsp;
+                                </p>
+                                <p>Turmas: {escola.turmas ? escola.turmas.length : '0'}</p>
+                            </div>
+                        </>
+                    )
+                }
             </div>
+
+            <ModalConfirmarExclusao
+                idModal={idModal}
+                titulo={`Excluir a escola ${escola.nome}?`}
+                mensagem={(
+                    <div>
+                        <p>Tem certeza de que deseja excluir a escola {escola.nome}?</p>
+                        <p>Também serão excluídos todas suas turmas e alunos.</p>
+                    </div>
+                )}
+                confirmar={props.excluir}
+            />
         </div>
     );
 }
--- web/src/Pages/AreaAdmin/Escola/lista-escola/ListaEscola.js
+++ web/src/Pages/AreaAdmin/Escola/lista-escola/ListaEscola.js
@@ -10,24 +10,41 @@ class ListaEscola extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            listaEscolas: []
+            listaEscolas: [],
+            escolaId: -1
         }
     }
 
     componentDidMount() {
         EscolaService.getList()
-            .then(lista => this.setState({listaEscolas : lista}))
+            .then(lista => this.setState({ listaEscolas: lista }))
             .catch(error => this.props.handleUnauthorized(error))
             .catch(error => PopUp.erro(error));
+    }
+
+    remover(id, idx) {
+        this.setState({ escolaId: id })
+        EscolaService.deleteEscola(id)
+            .then(() => {
+                let novaLista = this.state.listaEscolas.slice();
+                novaLista.splice(idx, 1);
+                this.setState({ listaEscolas: novaLista });
+                PopUp.sucesso('Escola removida com sucesso');
+            })
+            .catch(error => this.props.handleUnauthorized(error))
+            .catch(error => PopUp.erro('Erro para remover a escola'))
+            .finally(() => this.setState({ escolaId: -1 }));
     }
 
     render() {
         const lista = this.state.listaEscolas.slice();
         const listaCards = lista.map((escola, idx) => {
             return <EscolaCard key={idx} escola={escola}
-                        selecionar={() => this.props.selecionar(escola)}
-                        editar={() => this.props.editar(escola.id)}
-                    />
+                selecionar={() => this.props.selecionar(escola)}
+                editar={() => this.props.editar(escola.id)}
+                excluir={() => this.remover(escola.id, idx)}
+                loading={this.state.escolaId === escola.id}
+            />
         });
 
         return (
@@ -35,9 +52,9 @@ class ListaEscola extends Component {
                 <div className="row">
                     <div className="col right">
                         <NavLink to={Rotas.ESCOLA_NOVO}>
-                            <button 
+                            <button
                                 className="btn-floating btn-large waves-effect waves-light blue"
-                                >
+                            >
                                 <i className="material-icons">add</i>
                             </button>
                         </NavLink>
